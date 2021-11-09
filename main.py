22ee178860b950055294712352379dd435f68078
Arijit Ganguly <arijit.ganguly004@gmail.com>
2021-11-09 16:20:22 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 16:19:08 2021
+
+@author: Arijit Ganguly
+"""
---
+++ test.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 16:18:35 2021
+
+@author: Arijit Ganguly
+"""
